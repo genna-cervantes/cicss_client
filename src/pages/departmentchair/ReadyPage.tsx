646041b5732e7b4dc92d5e5dedff4fc0b9f3c1d7
genna-cervantes <gennacervantes9@gmail.com>
2025-04-04 16:08:24 +0800

    feat: ready page

--- src/pages/departmentchair/ReadyPage.tsx
+++ src/pages/departmentchair/ReadyPage.tsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const ReadyPage = () => {
+
+  const [csReady, setCSReady] = useState(false);
+  const [isReady, setISReady] = useState(false);
+  const [itReady, setITReady] = useState(false);
+
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const fetchReadyDepartments = async () => {
+      const res = await fetch('http://localhost:3000/schedule/ready/departments')
+      if (res.ok){
+        const data = await res.json();
+
+        setCSReady(data.csReady)
+        setISReady(data.isReady)
+        setITReady(data.itReady)
+      }
+    }
+    fetchReadyDepartments()
+  }, [])
+
+  const handleClick = () => {
+    navigate("/departmentchair/manual-edit")   
+  }
+
   return (
     <div>
-        <h1>CS READY</h1>
-        <h1>IT READY</h1>
-        <h1>IS READY</h1>
+        {csReady && <h1>CS READY</h1>}
+        {itReady && <h1>IT READY</h1>}
+        {isReady && <h1>IS READY</h1>}
+        {csReady && isReady && itReady && <button onClick={handleClick}>Go to manual editing</button>}
     </div>
   )
 }
